88089f9524d744a450eb0c5e9ee81267d2b0d460
Meseret Alemnew <84620197+meseret-ale@users.noreply.github.com>
2023-01-12 22:22:01 -0800

    Update print.py

--- python track/print.py
+++ python track/print.py
@@ -1,19 +1,10 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import Counter
+lists = []
 n = int(input())
-s = []
-for i in range(0,n):
-    r = input()
-    s.append(r)
-
-ins = []
-num = 0
-for i in range(0, len(s)):
-    count = 1
-    for j in range(i + 1, len(s)):
-        if s[i] == s[j]:
-            count += 1
-            num = j
-    ins.append(count)
-ins.remove(ins[num])
-print(len(ins))
-print(*ins)
+for i in range(n):
+    lists.append(input())
+dic = Counter(lists)
+print(len(dic))
+for key in dic:
+    print(dic.get(key),end=" ")
