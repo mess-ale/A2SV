b8cbb8c60ece49b84861d4755a3d7cf7f562f5a0
Meseret Alemnew <84620197+meseret-ale@users.noreply.github.com>
2023-01-14 08:37:24 -0800

    Create piling.py

---
+++ python track/piling.py
@@ -0,0 +1,22 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import deque
+
+def piling(d):
+    while True:
+        large = None
+        if d[-1] > d[0]:
+            large = d.pop()
+        else:
+            large = d.popleft()
+        if len(d) == 0:
+            return "Yes"
+        if d[-1] > large or d[0] > large:
+            return "No"
+            
+
+            
+for i in range(int(input())):
+    no_of_cubes = int(input())
+    d = deque(map(int, input().split()))
+    print(piling(d))
+    
