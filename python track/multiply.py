1e87ad41838f54a59ff404fc946a54e5b76489a8
Meseret Alemnew <84620197+meseret-ale@users.noreply.github.com>
2023-01-15 08:41:36 -0800

    Create multiply.py

---
+++ python track/multiply.py
@@ -0,0 +1,6 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        firstnum = int(num1)
+        secondnum = int(num2)
+        
+        return str(firstnum * secondnum)
