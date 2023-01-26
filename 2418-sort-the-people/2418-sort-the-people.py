bdb95773c9fe4dbe9e07394a393bb117253b47b7
Meseret Alemnew <84620197+meseret-ale@users.noreply.github.com>
2023-01-26 17:45:50 +0300

    edited sort the people

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,13 +1,25 @@
-class Solution:
-    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        dic = {}
-        for i in range(len(names)):
-            dic[heights[i]] = names[i]
+lis = list(map(int, input().split()))
+array2 = list(map(int, input().split()))
+array1 = list(map(int, input().split()))
+
+
+result = []
+i = 0
+j = 0
+count = 0
+while i <= len(array1)-1:
+    if j > len(array2)-1:
+        i += 1
+        result.append(count)
+    elif array1[i] > array2[j]:
+        count += 1
+        j += 1
+    else:
+        i += 1
+        result.append(count)
         
-        for i in range(len(names)):
-            for j in range(i+1, len(names)):
-                if heights[i] < heights[j]:
-                    heights[i], heights[j] = heights[j], heights[i]
-                    names[i], names[j] = names[j], names[i]
-                    
-        return names
+for i in range(0, len(result)):
+    print(result[i], end=" ")
+    
+    
+    
