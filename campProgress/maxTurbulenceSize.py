ab1b2fdc353a4f7dda8a38b5873373834b7acbdf
Meseret Alemnew <84620197+meseret-ale@users.noreply.github.com>
2023-02-24 04:06:46 -0800

    Update maxTurbulenceSize.py

--- campProgress/maxTurbulenceSize.py
+++ campProgress/maxTurbulenceSize.py
@@ -3,35 +3,30 @@ class Solution:
 
         max_val = 0
         count = 0
-        for i in range(len(arr)-1):
-            if i % 2 == 0:
-                if arr[i] < arr[i+1]:
-                    count += 1
-                    max_val = max(max_val, count)
-                else:
-                    count = 0
-
-            else:
-                if arr[i] > arr[i+1]:
-                    count += 1
-                    max_val = max(max_val, count)
-                else:
-                    count = 0
-
         max_val1 = 0
-        count = 0
+        count1 = 0
         for i in range(len(arr)-1):
             if i % 2 == 0:
-                if arr[i] > arr[i+1]:
-                    count += 1
-                    max_val1 = max(max_val1, count)
-                else:
-                    count = 0
-
-            else:
                 if arr[i] < arr[i+1]:
                     count += 1
-                    max_val1 = max(max_val1, count)
+                    max_val = max(max_val, count)
                 else:
                     count = 0
+                if arr[i] > arr[i+1]:
+                    count1 += 1
+                    max_val1 = max(max_val1, count1)
+                else:
+                    count1 = 0
+            else:
+                if arr[i] > arr[i+1]:
+                    count += 1
+                    max_val = max(max_val, count)
+                else:
+                    count = 0
+                if arr[i] < arr[i+1]:
+                    count1 += 1
+                    max_val1 = max(max_val1, count1)
+                else:
+                    count1 = 0
+
         return max(max_val+1, max_val1+1)
