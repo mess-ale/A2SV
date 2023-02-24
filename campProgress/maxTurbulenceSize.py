92a0ac7b88375db51e380cf0f296e79b99f3e801
Meseret Alemnew <84620197+meseret-ale@users.noreply.github.com>
2023-02-24 04:03:14 -0800

    Create maxTurbulenceSize.py

---
+++ campProgress/maxTurbulenceSize.py
@@ -0,0 +1,37 @@
+class Solution:
+    def maxTurbulenceSize(self, arr: List[int]) -> int:
+
+        max_val = 0
+        count = 0
+        for i in range(len(arr)-1):
+            if i % 2 == 0:
+                if arr[i] < arr[i+1]:
+                    count += 1
+                    max_val = max(max_val, count)
+                else:
+                    count = 0
+
+            else:
+                if arr[i] > arr[i+1]:
+                    count += 1
+                    max_val = max(max_val, count)
+                else:
+                    count = 0
+
+        max_val1 = 0
+        count = 0
+        for i in range(len(arr)-1):
+            if i % 2 == 0:
+                if arr[i] > arr[i+1]:
+                    count += 1
+                    max_val1 = max(max_val1, count)
+                else:
+                    count = 0
+
+            else:
+                if arr[i] < arr[i+1]:
+                    count += 1
+                    max_val1 = max(max_val1, count)
+                else:
+                    count = 0
+        return max(max_val+1, max_val1+1)
