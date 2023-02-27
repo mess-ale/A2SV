a0ad5e5fb6919d325303cfc2cb1a3cb77380ec0f
Meseret Alemnew <84620197+meseret-ale@users.noreply.github.com>
2023-02-27 05:34:46 -0800

    Create numberOfSubarrays.py

---
+++ campProgress/numberOfSubarrays.py
@@ -0,0 +1,19 @@
+class Solution:
+    def numberOfSubarrays(self, arr: List[int], k: int) -> int:
+       
+        count = {0:1}
+        prefix = []
+        prev = 0
+        for num in (arr):
+            if num%2 != 0:
+                prefix.append(prev + 1)
+                prev = prefix[-1]
+            else: prefix.append(prev)
+        for num in prefix:
+            count[num] = count.get(num,0) + 1
+        res = 0
+        for num in prefix:
+            if num >= k:
+                dif = num - k
+                res += count.get(dif, 0)
+        return res 
