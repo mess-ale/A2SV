764608643eb0bfb4d9e199fc45b2e53b09548384
Meseret Alemnew <84620197+meseret-ale@users.noreply.github.com>
2023-03-01 00:49:55 -0800

    added pascals-triangle-ii.python

--- pascals-triangle-ii.py
+++ pascals-triangle-ii.py
@@ -1,27 +1,15 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
         
-        def create(row, step):
-            if step == rowIndex:
-                return row
+        def recreation(row):
+            if row < 2:
+                return [1]*(row+1)
+            curr = recreation(row-1)
+            
+            val = [1]
+            for i in range(1,row):
+                val.append(curr[i-1]+curr[i])
+            val.append(1)
+            return val
 
-
-
-            level = [1]
-            if len(row) >= 2:
-                for j in range(1,len(row)):
-                    level.append(row[j-1] + row[j])
-
-            level.append(1)
-            return create(level, step + 1)
-        return create([1], 0)
-        
-        # arr = [[1]]
-        # for i in range(rowIndex):
-        #     level = [1]
-        #     if len(arr[-1]) >= 2:
-        #         for j in range(1,len(arr[-1])):
-        #             level.append(arr[-1][j-1] + arr[-1][j])
-
-        #     level.append(1)
-        #     arr.append(level)
+        return recreation(rowIndex)
