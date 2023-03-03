6894606bc795b70bb91e4b2388d259c7c4811bbf
Meseret Alemnew <84620197+meseret-ale@users.noreply.github.com>
2023-03-03 03:29:54 -0800

    added sqrtx.python

---
+++ sqrtx.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+
+        left = 0
+        right = x
+        mid = 0
+        while left <= right:
+            mid = left + (right - left)//2
+            if mid*mid > x:
+                right = mid - 1
+            elif mid*mid < x:
+                left = mid + 1
+            else:
+                return mid
+
+        return right
