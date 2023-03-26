33e7f28a9b34c84e942b46cd94fc29337fc8b60e
Meseret Alemnew <84620197+meseret-ale@users.noreply.github.com>
2023-03-26 10:53:29 -0700

    added online-election.python

---
+++ online-election.py
@@ -0,0 +1,18 @@
+class TopVotedCandidate:
+
+    def __init__(self, persons, times):
+        self.leads, self.times, count = [], times, {}
+        lead = -1
+        for p in persons:
+            count[p] = count.get(p, 0) + 1
+            if count[p] >= count.get(lead, 0): lead = p
+            self.leads.append(lead)
+
+    def q(self, t):
+        return self.leads[bisect.bisect(self.times, t) - 1]
+        
+
+
+# Your TopVotedCandidate object will be instantiated and called as such:
+# obj = TopVotedCandidate(persons, times)
+# param_1 = obj.q(t)
